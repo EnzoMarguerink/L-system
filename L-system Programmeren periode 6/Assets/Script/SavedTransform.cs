dc64a82633fd55a1b23eda7c63749216cc3d31b9
enzo marguerink <enzomarquerink@gmail.com>
2020-02-23 19:39:59 +0100

    klaar

--- L-system Programmeren periode 6/Assets/Script/SavedTransform.cs
+++ L-system Programmeren periode 6/Assets/Script/SavedTransform.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public struct SavedTransform
-{
-    public Vector3 position;
-    public Quaternion rotation;
-}
+//public struct SavedTransform
+//{
+//    public Vector3 position;
+//    public Quaternion rotation;
+//}
--- L-system Programmeren periode 6/Assets/Script/TreeElement.cs
+++ L-system Programmeren periode 6/Assets/Script/TreeElement.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class TreeElement : MonoBehaviour
-{
-    public LineRenderer lineRenderer;
-    public Material material;
-}
+//public class TreeElement : MonoBehaviour
+//{
+//    public LineRenderer lineRenderer;
+//    public Material material;
+//}
